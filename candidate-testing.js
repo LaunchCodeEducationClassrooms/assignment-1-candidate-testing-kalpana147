c9041504d323b2efb44fcb3a7923b5177d83e7f2
kalpana14 <kalpana.chelladurai14@gmail.com>
2021-02-17 22:38:45 +0000

    updated all the lines to get the desired output.

--- candidate-testing.js
+++ candidate-testing.js
@@ -8,42 +8,46 @@ let candidateName="";
 let question="Who was the first American woman in space? ";
 let correctAnswer="Sally Ride";
 let candidateAnswer="";
-let questions=["True or false: 5000 meters = 5 kilometers.","(5 + 3)/2 * 10 = ?","Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2?","What is the minimum crew size for the ISS?"];
-let correctAnswers=["True","40","Trajectory","3"];
+let questions=["Who was the first American woman in space? ","True or false: 5000 meters = 5 kilometers.","(5 + 3)/2 * 10 = ?","Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2?","What is the minimum crew size for the ISS?"];
+let correctAnswers=["Sally ride","True","40","Trajectory","3"];
 let candidateAnswers=[];
 let questionNumber=1;
 let score=0;
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
-candidateName=input.question("Candidate Name:");
+candidateName = input.question("Candidate Name: ");
+candidateName = String(candidateName);
 }
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-candidateAnswer=input.question((questionNumber ) + ") "+ question+"\nYour answer: ");
-console.log("Correct Answer: "+ correctAnswer + "\n")
-for (let i = 0; i < questions.length; i++){  candidateAnswers[i] = input.question((questionNumber+1 + i) + ") "+ questions[i]+"\nYour answer: ");
-  console.log("Correct Answer: "+ correctAnswers[i]+ "\n")
+for (let i = 0; i < questions.length; i++) {
+    candidateAnswer = input.question((i+1) + ") " + questions[i]);
+    candidateAnswers.push(candidateAnswer);
+    console.log(`Your Answer: ${candidateAnswers[i]}\nCorrect Answer: ${correctAnswers[i]}\n`);
+
 } 
 }
 
 function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
-if(candidateAnswer.toLowerCase()=== correctAnswer.toLowerCase()){
-score +=1;}
-for (let i = 0; i < questions.length; i++){
-if(candidateAnswers[i].toLowerCase()=== correctAnswers[i].toLowerCase()){
-score +=1;
- }
-}
-  let grade;
-grade= score/(1+questions.length)*100;
-  if(grade>=80){result="PASSED";
-  }else{
-    result="FAILED";
+let grade;
+  let score = 0;
+  
+  for (let i = 0; i < questions.length; i++) {
+    if (candidateAnswers[i].toLowerCase() === correctAnswers[i].toLowerCase()){
+     score += 1;
+    }
   }
-  console.log(`>>> Overall Grade: ${grade}% (${score} of ${questions.length+1} responses correct) <<< \n>>> Status: ${result} <<<`);
+  console.log(`\nYou answered ${score} questions correctly.`);
+  grade = (score / questions.length) * 100;
+  if (grade >= 80) {
+    console.log(`You PASSED with a score of ${grade}%.`);
+  } else {
+    console.log(`You FAILED with a score of ${grade}%.`);
+  }
+  return grade;
 
   return grade;
 }
